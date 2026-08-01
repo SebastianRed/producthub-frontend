5ce2558fa41d2e4d11f0ce357969e6856afc033d
Sebastián Rojo <sebastianrojocollao@gmail.com>
2026-07-31 23:25:02 -0400

    Agregar navegación en MainLayout con enlaces a Home, Productos y Categorías

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,7 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 function MainLayout() {
-  return <Outlet />;
+  return (
+    <>
+      <nav>
+        <NavLink to="/">Home</NavLink>{" | "}
+        <NavLink to="/products">Productos</NavLink>{" | "}
+        <NavLink to="/categories">Categorías</NavLink>
+      </nav>
+
+      <hr />
+
+      <Outlet />
+    </>
+  );
 }
 
 export default MainLayout;
